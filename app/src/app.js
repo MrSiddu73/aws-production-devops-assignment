2df8a7869b43f24c8677aff9f44a3a8e098987d0
MrSiddu73 <siddusnyamagoud27@gmail.com>
2026-07-06 18:04:46 +0530

    Configure application file logging

--- app/src/app.js
+++ app/src/app.js
@@ -1,5 +1,19 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const helmet = require("helmet");
+
+const logDirectory = path.join(__dirname, "../logs");
+
+if (!fs.existsSync(logDirectory)) {
+    fs.mkdirSync(logDirectory, { recursive: true });
+}
+
+const accessLogStream = fs.createWriteStream(
+    path.join(logDirectory, "access.log"),
+    { flags: "a" }
+);
+
 const morgan = require("morgan");
 const cors = require("cors");
 const compression = require("compression");
@@ -12,8 +26,13 @@ app.use(helmet());
 app.use(cors());
 app.use(compression());
 app.use(express.json());
-app.use(morgan("combined"));
+app.use(morgan("dev"));
 
+app.use(
+    morgan("combined", {
+        stream: accessLogStream,
+    })
+);
 app.use("/", routes);
 
 module.exports = app;
